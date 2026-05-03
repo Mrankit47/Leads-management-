397c0a8e503d9cd9053cb7c75601f6729148688f
Ankit Kushwah <ankitkushwah0210k@gmail.com>
2026-05-04 01:35:56 +0530

    final

--- leadgen/settings.py
+++ leadgen/settings.py
@@ -196,16 +196,3 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-import os
-from django.contrib.auth import get_user_model
-
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    User = get_user_model()
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-    if username and password and not User.objects.filter(username=username).exists():
-        print("Creating superuser...")
-        User.objects.create_superuser(username=username, email=email, password=password)
